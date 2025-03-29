6534c50850659fd191aeac63c212cae0f55fe968
gwangbu-desu <kwy1379@ajou.ac.kr>
2025-03-29 18:51:45 +0900

    Fix: Eureka에 등록할 서비스명 추가

--- src/main/java/com/cryptory/be/BeApplication.java
+++ src/main/java/com/cryptory/be/BeApplication.java
@@ -1,15 +1,22 @@
 package com.cryptory.be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class BeApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// .env에서 값을 가져와 시스템 환경 변수로 설정
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(BeApplication.class, args);
 	}
 
